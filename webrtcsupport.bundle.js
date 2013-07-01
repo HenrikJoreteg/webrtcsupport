230a21a7b06d3b60683b16ea25c818d6cc7689b9
Henrik Joreteg <hjoreteg@gmail.com>
2013-07-01 12:47:29 -0700

    fixes critical FF support bug

--- webrtcsupport.bundle.js
+++ webrtcsupport.bundle.js
@@ -1,9 +1,9 @@
 (function(e){if("function"==typeof bootstrap)bootstrap("webrtcsupport",e);else if("object"==typeof exports)module.exports=e();else if("function"==typeof define&&define.amd)define(e);else if("undefined"!=typeof ses){if(!ses.ok())return;ses.makeWebrtcsupport=e}else"undefined"!=typeof window?window.webrtcsupport=e():global.webrtcsupport=e()})(function(){var define,ses,bootstrap,module,exports;
 return (function(e,t,n){function i(n,s){if(!t[n]){if(!e[n]){var o=typeof require=="function"&&require;if(!s&&o)return o(n,!0);if(r)return r(n,!0);throw new Error("Cannot find module '"+n+"'")}var u=t[n]={exports:{}};e[n][0].call(u.exports,function(t){var r=e[n][1][t];return i(r?r:t)},u,u.exports)}return t[n].exports}var r=typeof require=="function"&&require;for(var s=0;s<n.length;s++)i(n[s]);return i})({1:[function(require,module,exports){
 // created by @HenrikJoreteg
-var PC = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-var IceCandidate = window.RTCIceCandidate || window.mozRTCIceCandidate;
-var SessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription;
+var PC = window.mozRTCPeerConnection || window.webkitRTCPeerConnection || window.RTCPeerConnection;
+var IceCandidate = window.mozRTCIceCandidate || window.RTCIceCandidate;
+var SessionDescription = window.mozRTCSessionDescription || window.RTCSessionDescription;
 var prefix = function () {
     if (window.mozRTCPeerConnection) {
         return 'moz';
@@ -12,13 +12,16 @@ var prefix = function () {
     }
 }();
 var screenSharing = navigator.userAgent.match('Chrome') && parseInt(navigator.userAgent.match(/Chrome\/(.*) /)[1], 10) >= 26;
+var webAudio = !!window.webkitAudioContext;
 
-// export support flags and constructors
+// export support flags and constructors.prototype && PC
 module.exports = {
     support: !!PC,
-    dataChannel: !!(PC && PC.prototype.createDataChannel),
+    dataChannel: !!(PC && PC.prototype && PC.prototype.createDataChannel),
     prefix: prefix,
+    webAudio: webAudio,
     screenSharing: screenSharing,
+    PeerConnection: PC,
     SessionDescription: SessionDescription,
     IceCandidate: IceCandidate
 };
--- webrtcsupport.js
+++ webrtcsupport.js
@@ -1,7 +1,7 @@
 // created by @HenrikJoreteg
-var PC = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
-var IceCandidate = window.RTCIceCandidate || window.mozRTCIceCandidate;
-var SessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription;
+var PC = window.mozRTCPeerConnection || window.webkitRTCPeerConnection || window.RTCPeerConnection;
+var IceCandidate = window.mozRTCIceCandidate || window.RTCIceCandidate;
+var SessionDescription = window.mozRTCSessionDescription || window.RTCSessionDescription;
 var prefix = function () {
     if (window.mozRTCPeerConnection) {
         return 'moz';
@@ -12,10 +12,10 @@ var prefix = function () {
 var screenSharing = navigator.userAgent.match('Chrome') && parseInt(navigator.userAgent.match(/Chrome\/(.*) /)[1], 10) >= 26;
 var webAudio = !!window.webkitAudioContext;
 
-// export support flags and constructors
+// export support flags and constructors.prototype && PC
 module.exports = {
     support: !!PC,
-    dataChannel: !!(PC && PC.prototype.createDataChannel),
+    dataChannel: !!(PC && PC.prototype && PC.prototype.createDataChannel),
     prefix: prefix,
     webAudio: webAudio,
     screenSharing: screenSharing,
