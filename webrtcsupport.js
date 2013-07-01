6a6b706e6e4c0f3fff64e2cc4f8d5c235f157baa
Henrik Joreteg <hjoreteg@gmail.com>
2013-07-01 09:07:06 -0700

    adding prefix detection support

--- webrtcsupport.js
+++ webrtcsupport.js
@@ -2,6 +2,13 @@
 var PC = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
 var IceCandidate = window.RTCIceCandidate || window.mozRTCIceCandidate;
 var SessionDescription = window.RTCSessionDescription || window.mozRTCSessionDescription;
+var prefix = function () {
+    if (window.mozRTCPeerConnection) {
+        return 'moz';
+    } else if (window.webkitRTCPeerConnection) {
+        return 'webkit';
+    }
+}();
 
 // export support flags and constructors
 module.exports = {
@@ -9,5 +16,6 @@ module.exports = {
     SessionDescription: SessionDescription,
     IceCandidate: IceCandidate,
     support: !!PC,
-    dataChannel: !!(PC && PC.prototype.createDataChannel)
+    dataChannel: !!(PC && PC.prototype.createDataChannel),
+    prefix: prefix
 };
