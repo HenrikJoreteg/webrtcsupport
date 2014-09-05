fcf9eaea036249845dfbf15106ef9beacbf4ab47
Lance Stout <lancestout@gmail.com>
2014-09-05 10:41:59 -0700

    Export the MediaStream class

--- index-browser.js
+++ index-browser.js
@@ -17,7 +17,7 @@ var PC = window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
 var IceCandidate = window.mozRTCIceCandidate || window.RTCIceCandidate;
 var SessionDescription = window.mozRTCSessionDescription || window.RTCSessionDescription;
 var MediaStream = window.webkitMediaStream || window.MediaStream;
-var screenSharing = window.location.protocol === 'https:' && 
+var screenSharing = window.location.protocol === 'https:' &&
     ((window.navigator.userAgent.match('Chrome') && parseInt(window.navigator.userAgent.match(/Chrome\/(.*) /)[1], 10) >= 26) ||
      (window.navigator.userAgent.match('Firefox') && parseInt(window.navigator.userAgent.match(/Firefox\/(.*)/)[1], 10) >= 33));
 var AudioContext = window.webkitAudioContext || window.AudioContext;
@@ -34,5 +34,6 @@ module.exports = {
     AudioContext: AudioContext,
     PeerConnection: PC,
     SessionDescription: SessionDescription,
-    IceCandidate: IceCandidate
+    IceCandidate: IceCandidate,
+    MediaStream: MediaStream
 };
